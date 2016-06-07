044321b837378e3030c98088bcd109cb290848b8
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2016-06-07 16:28:58 +0200

    Add command.py which allows to run external commands and capture stdout and stderr.
    Add command.py which allows to run external commands and capture stdout and stderr.
    
    Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.be>

---
+++ command.py
@@ -0,0 +1,47 @@
+"""
+Purpose :      Run external commands.
+
+Copyright (C): 2016 - Gert Hulselmans
+"""
+
+from __future__ import print_function
+
+import subprocess
+import sys
+
+
+def run_cmd(cmd, stdin=None):
+    """
+    Execute the passed command line and exit when the command failed.
+
+    :param cmd: list of command line parameters.
+    :param stdin: Standard input string or None.
+    :return: stdout_data, stderr_data (on error print stdout_data and stderr_data to standard error)
+    """
+
+    try:
+        pid = subprocess.Popen(args=cmd,
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=False)
+        stdout_data, stderr_data = pid.communicate(stdin)
+    except OSError as msg:
+        print("\nERROR during execution of '" + ' '.join(cmd) + "': " + str(msg), file=sys.stderr)
+        sys.exit(1)
+
+    if pid.returncode != 0:
+        print("\nERROR during execution of '" + ' '.join(cmd) + "':", file=sys.stderr)
+        print("\nStandard output:\n----------------", file=sys.stderr)
+        print(stdout_data, file=sys.stderr)
+        print("\nStandard error:\n---------------", file=sys.stderr)
+        print(stderr_data, file=sys.stderr)
+
+        print("\nERROR during execution of '" + ' '.join(cmd) + "':", file=sys.stderr)
+        print("\nStandard output:\n----------------", file=sys.stderr)
+        print(stdout_data, file=sys.stderr)
+        print("\nStandard error:\n---------------", file=sys.stderr)
+        print(stderr_data, file=sys.stderr)
+        sys.exit(1)
+
+    return stdout_data, stderr_data
