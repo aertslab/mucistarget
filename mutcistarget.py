aertslab/mucistarget

commit 6a479bd6772770c17a0de522cae584a6c8ab8de2

If a set of genes was provided, only keep those associated genes for the mutation that appear in this set of genes.
If a set of genes was provided, only keep those associated genes
for the mutation that appear in this set of genes instead of
keeping all associated genes for a mutation.

Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.be>

mutcistarget.py
@@ -120,10 +120,18 @@ def get_all_mutations_that_overlap_with_regdoms_of_genes(vcf_mut_iterator, genes
 
             associated_genes_and_distance_to_tss_dict = vcf_mut.get_associated_genes_and_distance_to_tss()
 
-            # Store mutation if one of the following conditions if fulfilled:
-            #   - no set of genes was provided.
-            #   - a set of genes was provided and the associated genes for the mutation are in this list.
-            if not genes_set or not set(associated_genes_and_distance_to_tss_dict).isdisjoint(genes_set):
+            if genes_set and not set(associated_genes_and_distance_to_tss_dict).isdisjoint(genes_set):
+                # If a set of genes was provided, only keep those associated genes for the mutation that appear in this
+                # set of genes.
+                associated_genes_and_distance_to_tss_dict = (
+                    {associated_genes_and_distance_to_tss_key: associated_genes_and_distance_to_tss_dict[
+                        associated_genes_and_distance_to_tss_key]
+                     for associated_genes_and_distance_to_tss_key in associated_genes_and_distance_to_tss_dict
+                     if associated_genes_and_distance_to_tss_key in genes_set
+                     }
+                )
+
+            if associated_genes_and_distance_to_tss_dict:
                 # Store all mutations (VCFmut object) and associated genes information in a ordered dict.
                 vcf_mut_to_associated_genes_and_distance_to_tss_dict[vcf_mut] \
                     = associated_genes_and_distance_to_tss_dict

commit efd01dadbc66381752f5d32988c88c0dddda1003

Fix filtering of mutations based on associated genes when "--genes" option is provided.
Fix filtering of mutations based associated genes when "--genes"
option is provided.

It was accidentially broken in commit 6a479bd6772770c17a0de522cae584a6c8ab8de2:

    If a set of genes was provided, only keep those associated genes
    for the mutation that appear in this set of genes.

due a wrong if condition which caused that mutations that had
associated genes which where not in the set of genes, where not
removed.

Signed-off-by: Gert Hulselmans <gert.hulselmans@kuleuven.be>

mutcistarget.py
@@ -120,7 +120,7 @@ def get_all_mutations_that_overlap_with_regdoms_of_genes(vcf_mut_iterator, genes
 
             associated_genes_and_distance_to_tss_dict = vcf_mut.get_associated_genes_and_distance_to_tss()
 
-            if genes_set and not set(associated_genes_and_distance_to_tss_dict).isdisjoint(genes_set):
+            if genes_set:
                 # If a set of genes was provided, only keep those associated genes for the mutation that appear in this
                 # set of genes.
                 associated_genes_and_distance_to_tss_dict = (